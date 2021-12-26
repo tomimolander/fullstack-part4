3fdf5c932355af717629b15e65b640390356a2d5
unknown <tomi.molander@aalto.fi>
2021-12-26 10:40:43 +0200

    part 4 excercises

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,21 @@
 // eslint-disable-next-line new-cap
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
+/*
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+*/
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  // const blogs = await Blog.find({})
   response.json(blogs.map((blog) => blog.toJSON()))
   /*
   Blog
@@ -15,9 +27,32 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  // const user = await User.findById(body.userId)
+
+  // const token = getTokenFrom(request)
+  /*
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  */
+  const user = request.user
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  })
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.json(savedBlog.toJSON())
+
   /*
   blog
     .save()
@@ -28,6 +63,20 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  /*
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  */
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if ( blog.user.toString() !== user._id.toString() ) {
+    return response.status(401).json({
+      error: 'blog not created by the user and cannot be removed' })
+  }
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -19,13 +21,46 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token',
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired',
+    })
   }
 
+
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  /*
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }*/
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (request.token && decodedToken.id) {
+    request.user = await User.findById(decodedToken.id)
+  }
+  // const user = await User.findById(decodedToken.id)
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor,
+  userExtractor,
 }
